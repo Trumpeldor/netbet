5acbb9501f1ecaed3020eef831aea5e9cf273818
Amit Rosner <amit_rosner@yahoo.com>
2021-02-16 23:52:52 +0200

    Add Lotteries

---
+++ client/src/components/Lotteries/Lottery.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import Utils from '../../utils/Utils';
+import Actions from '../../store/actions/lotteries';
+import './index.css';
+
+function Lottery({ lottery: { event, prize, currency, date, id, millis }, remove }) {
+	const [timeLeft, setTimeLeft] = useState(millis - new Date().getTime());
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Mock: call some API to play now in ' + event + '!');
+  };
+  useEffect(() => {
+    let isMounted = true;
+    const timer = setTimeout(() => {
+      if (isMounted) {
+        if (timeLeft > 1000) {
+          setTimeLeft(timeLeft - 1000);
+        } else {
+          clearTimeout(timer);
+          remove(date);
+        }
+      }
+    }, 1000);
+    return () => { isMounted = false };
+  });
+  const parts = Utils.parseDuration(timeLeft);
+  const days = parts.days > 0 ? parts.days > 1 ? parts.days + ' Days ' : '1 Day ' : '';
+  const withZero = (part) => {
+    return part < 10 ? '0' + part : part;
+  };
+  const time = withZero(parts.hours) + ':' + withZero(parts.minutes) + ':' + withZero(parts.seconds);
+  return (
+    <form onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>{event}</td>
+          </tr>
+          <tr>
+            <td>
+              <label>{currency}</label>
+              <label className="prize">{prize}</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input title='Play Now' type='submit' value='Play Now' />
+            </td>
+          </tr>
+          <tr>
+            <td>{days}{time}</td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+
+export default connect(
+  null,
+  dispatch => ({
+    remove: (date) => dispatch(Actions.remove(date))
+  })
+)(Lottery)
---
+++ client/src/components/Lotteries/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import Lottery from './Lottery';
+import Utils from '../../utils/Utils';
+import ItemsCarousel from 'react-items-carousel';
+import './index.css';
+
+function Lotteries({ lotteries }) {
+	const [activeItemIndex, setActiveItemIndex] = useState(0);
+  if (activeItemIndex > 0 && !Utils.isLongerArray(lotteries, 0)) {
+    setActiveItemIndex(0);
+  }
+  const now = new Date().getTime();
+  return (
+    <div className="ads">
+      <ItemsCarousel
+        numberOfCards={4}
+        gutter={12}
+        showSlither={true}
+        firstAndLastGutter={true}
+        freeScrolling={false}
+        requestToChangeActive={setActiveItemIndex}
+        activeItemIndex={activeItemIndex}
+        activePosition={'center'}
+        chevronWidth={24}
+        rightChevron={'>'}
+        leftChevron={'<'}
+        outsideChevron={false}
+      >
+      {lotteries.filter(lottery => lottery.millis > now).map((lottery) => {
+        return (
+          <Lottery lottery={lottery} key={lottery.id} />
+        );
+      })}
+      </ItemsCarousel>
+    </div>
+  );
+}
+
+export default connect(
+  state => ({
+    lotteries: state.lotteries
+  })
+)(Lotteries)
